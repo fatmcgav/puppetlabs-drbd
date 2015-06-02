0e2d4bdcc7243e883c3049278fdc207edb9d3f7f
David Mahony (C) <David.Mahony@os.uk>
2015-06-02 09:32:30 +0100

    Ensuring that the node is only promoted to primary when the other node is not primary and both uptodate

--- manifests/resource/up.pp
+++ manifests/resource/up.pp
@@ -62,6 +62,7 @@ define drbd::resource::up (
     exec { "drbd_make_primary_again_${name}":
       command => "drbdadm primary ${name}",
       unless  => "drbdadm role ${name} | egrep '^Primary'",
+      onlyif  => "cat /proc/drbd | egrep \"ro:Secondary/Secondary ds:UpToDate/UpToDate\"",
       require => Service['drbd'],
     }
 
